84358229876e0c93dc8d14c2af77edaf5bdfb2a4
dev-prime99 <prime99@icloud.com>
2019-10-15 14:24:20 +0200

    Added and tested js. Right click locked

---
+++ assets/js/javascript.js
@@ -0,0 +1,11 @@
+document.getElementById('bocca').innerHTML='Sciacqua la Bocca';
+document.getElementById('bocca2').innerHTML='Sciacqua la Bocca';
+document.getElementById('bocca3').innerHTML='Sciacqua la Bocca';
+
+document.getElementById('night').innerHTML='Night Skinny';
+document.getElementById('night2').innerHTML='Night Skinny';
+
+document.getElementById('chadia').innerHTML='Chadia Rodriguez';
+document.getElementById('chadia2').innerHTML='Chadia Rodriguez';
+
+document.getElementById('cantanti').innerHTML='Night Skinny, Chadia Rodriguez';
